f3919466dedc84f61625f97e7f1f67016b89e575
hiropiyo <hh.hiro.book@gmail.com>
2022-05-07 19:48:02 +0900

    いったん完成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,85 @@
-import "./styles.css";
+// import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
+  // li生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  // span生成
+  const span = document.createElement("span");
+  span.innerText = text;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    // TODO内容テキストを取得
+    const text = addTarget.firstChild.innerText;
+
+    // li以下を初期化
+    addTarget.textContent = null;
+
+    // span生成
+    const span = document.createElement("span");
+    span.innerText = text;
+
+    // button(戻す)タグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(div)を完了リストから削除
+      const backTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(backTarget);
+
+      // テキストを取得
+      const text = backTarget.firstChild.innerText;
+
+      // 未完了リストに追加
+      createIncompleteList(text);
+    });
+
+    // liタグの子要素に各要素を設定
+    addTarget.appendChild(span);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(li)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(span);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
